546518f9fdc377d8b033592e51df74f8ba2ce7c7
ohnmar <ohnmaraungsg@gmail.com>
2022-12-17 22:35:47 +0800

    just add loop count

--- bubbleSort.py
+++ bubbleSort.py
@@ -2,11 +2,12 @@ def bubbleSort(arr):
     iteration = 0
     for i in range(0,len(arr)):
         for j in range(0,len(arr)-1-i):
+            iteration += 1
             if arr[j] > arr[j+1]:
                 temp = arr[j]
                 arr[j] = arr[j+1]
                 arr[j+1] = temp 
-    return arr
+    return arr, iteration
 
 A = [5,1,9,10,2,3]
 print(bubbleSort(A))
