51062f64e19393edbe00ca48622197559cda9770
ohnmar <ohnmaraungsg@gmail.com>
2022-12-26 12:20:01 +0800

    check type and value error

--- unitest/prime/TestPrime.py
+++ unitest/prime/TestPrime.py
@@ -15,5 +15,25 @@ class TestPrime(unittest.TestCase):
     def test_eleven(self):
         self.assertTrue(is_prime(11))
 
+    #or as below
+
+    def test_prime_not_prime(self):
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(5))
+        self.assertFalse(is_prime(9))
+        self.assertTrue(is_prime(11))
+
+    def test_typeerror_1(self):
+        with self.assertRaises(TypeError):
+            is_prime(1.5)
+
+    def test_typeerror_2(self):
+        with self.assertRaises(TypeError):
+            is_prime("one")
+
+    def test_valueerror(self):
+        with self.assertRaises(ValueError):
+            is_prime(-4)
+
 if __name__=='__main__':
     unittest.main()
--- unitest/prime/prime_number.py
+++ unitest/prime/prime_number.py
@@ -1,9 +1,14 @@
 import math
 def is_prime(num):
+    if type(num)!=int:
+        raise TypeError("invalid type")
     
+    if num < 0:
+        raise ValueError("Num is negative value")
+
     for i in range(2,int(math.sqrt(num)+1)):
         if num%i==0:
             return False  
     return True 
 
-print(is_prime(29))
+print(is_prime(5))
